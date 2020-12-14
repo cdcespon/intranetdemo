12f437a473ebeef53a5f1f2e34bf331d304450bc
cdcespon <cdcespon@gmail.com>
2020-12-13 23:16:09 -0300

    scripts

---
+++ config.js
@@ -0,0 +1,51 @@
+﻿
+const webservicesurl = "https://cdcespon.github.io/intranetdemo//WebServices/"
+
+function getwebservicesurl() {
+    return 'https://cdcespon.github.io/intranetdemo//WebServices/'
+}
+
+function getresourcesurl() {
+    return 'https://cdcespon.github.io/intranetdemo/Resources/'
+}
+
+function getlocalurl() {
+    return 'https://cdcespon.github.io/intranetdemo/'
+}
+
+function getwebapiurl() {
+   return 'http://localhost:5696/'
+}
+
+function loadScripts(group) {
+    var res = group.split(',')
+    for (var i = 0; i < res.length; i++) {
+        switch (res[i]) {
+            case 'login':
+                $('body').append('<script type="text/javascript" src="Scripts/login.js"/>');
+                $('body').append('<script type="text/javascript" src="' + getlocalurl() + 'Scripts/MD5.js"/>');
+                $('body').append('<script type="text/javascript" src= "' + getresourcesurl() + 'jQuery/plugins/iCheck/icheck.min.js"/>');
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+
+function getData(serviceUrl, jsonData, isAsync, beforeSendCallback, successCallback, errorCallback, completeCallback) {
+
+    $.ajax({
+        url: webservicesurl + serviceUrl,
+        data: jsonData,
+        type: 'POST',
+        dataType: 'json',
+        async: isAsync,
+        crossdomain: true,
+        beforeSend: beforeSendCallback,
+        success: successCallback,
+        error: errorCallback,
+        complete: completeCallback,
+    });
+}
+
---
+++ global.js
@@ -0,0 +1,42 @@
+﻿const webservicesurl = "https://cdcespon.github.io/intranetdemo//WebServices/"
+const resourcesurl = "https://cdcespon.github.io/intranetdemo//Resources/"
+const localurl = "https://cdcespon.github.io/intranetdemo/"
+const webapiurl= "http://localhost:5696/"
+
+
+function getData(serviceUrl, jsonData, isAsync, beforeSendCallback, successCallback, errorCallback, completeCallback) {
+
+    $.ajax({
+        url: webservicesurl + serviceUrl,
+        data: jsonData,
+        type: 'POST',
+        dataType: 'json',
+        async: isAsync,
+        crossdomain: true,
+        beforeSend: beforeSendCallback,
+        success: successCallback,
+        error: errorCallback,
+        complete: completeCallback,
+    });
+}
+
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
+
+
+function loadScripts(group) {
+    var res = group.split(',')
+    for (var i = 0; i < res.length; i++) {
+        switch (res[i]) {
+            case 'login':
+                $('body').append('<script type="text/javascript" src="Scripts/login.js"/>');
+                $('body').append('<script type="text/javascript" src="' + localurl + 'Scripts/MD5.js"/>');
+                $('body').append('<script type="text/javascript" src= "' + resourcesurl + 'jQuery/plugins/iCheck/icheck.min.js"/>');
+                break;
+            default:
+                break;
+        }
+    }
+}
+
